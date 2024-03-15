6ec7214785c658160a9aa78fe3cf352198614d6b
haroldcarrillo <161344555+haroldcarrillo@users.noreply.github.com>
2024-03-14 21:23:40 -0500

    Update encuesta.php

--- controladores/encuesta.php
+++ controladores/encuesta.php
@@ -1,8 +1,22 @@
 <?php
+require "../confing/conexion2.php";
 
 $excelente = $_POST["calificacion1"];
 $calificacion2 = $_POST["calificacion2"];
 $calificacion = $_POST["calificacion3"];
 $calificacion4 = $_POST["calificacion4"]
 
+$sql_insertar= "INSERT INTO encuesta
+(fecha_sys, excelente, calificacion2,calificacion,calificacion4) 
+VALUES (now(),'".$excelente."','".$calificacion2."','".$calificacion."','".$calificacion4."')";
+
+
+//ENVIAR A LA BD
+if($dbh->query($sql_insertar))
+{
+    print "INFORMACION REGISTRADA CORRECTAMENTE";
+}else
+{
+    print "ERROR AL REGISTRAR LA INFOPRMACION";
+}
 ?>

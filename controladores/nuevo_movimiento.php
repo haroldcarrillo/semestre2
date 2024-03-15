d7edad40b37cdc5123c784dfbca0b02231f709b9
haroldcarrillo <161344555+haroldcarrillo@users.noreply.github.com>
2024-03-14 21:17:34 -0500

    Update nuevo_movimiento.php

--- controladores/nuevo_movimiento.php
+++ controladores/nuevo_movimiento.php
@@ -1,11 +1,28 @@
 <?php
 
+require "../confing/conexion.php";
+
 $fechas = $_POST["fecha"];
 $horas = $_POST["hora"];
 $conceptos = $_POST["concepto"];
 
 $cantidades = $_POST["cantidad"];
 
-print "$horas";
+$sql_insertar = "INSERT nuevo_movimiento
+(fecha_sys,fecha,hora,concepto,cantidad)
+VALUES (now(),'".$fechas."','".$horas."','".$conceptos."','".$cantidades."')";
+
+
+
+
+
+//ENVIAR A LA BD
+if($dbh->query($sql_insertar))
+{
+    print "INFORMACION REGISTRADA CORRECTAMENTE";
+}else
+{
+    print "ERROR AL REGISTRAR LA INFORMACION";
+}
 
 ?>

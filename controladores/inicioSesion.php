19ee086ef8709c696d68cd84a7e48f79d79a2e3a
haroldcarrillo <161344555+haroldcarrillo@users.noreply.github.com>
2024-03-14 21:18:36 -0500

    Update inicioSesion.php

--- controladores/inicioSesion.php
+++ controladores/inicioSesion.php
@@ -1,7 +1,21 @@
 <?php
 
+require "../confing/conexion2.php";
+
 $usuario = $_POST["usuario"];
 $password =$_POST["password"];
 
+$sql_insertar= "INSERT INTO inicio_sesion
+(fecha_sys, usuario, password) 
+VALUES (now(),'".$usuario."','".$password."')";
 
-?>
+
+
+//ENVIAR A LA BD
+if($dbh->query($sql_insertar))
+{
+    print "INFORMACION REGISTRADA CORRECTAMENTE";
+}else
+{
+    print "ERROR AL REGISTRAR LA INFOPRMACION";
+}

06c862b665697e4b661f7028388ebcd2aeec1eb8
haroldcarrillo <161344555+haroldcarrillo@users.noreply.github.com>
2024-03-14 21:16:16 -0500

    Update nuevo_usuario.php

--- controladores/nuevo_usuario.php
+++ controladores/nuevo_usuario.php
@@ -1,5 +1,8 @@
 <?php
 
+require "../confing/conexion.php";
+
+
 $usuario = $_POST["usuario"];
 $password = $_POST["password"];
 $email = $_POST["correo"];
@@ -9,4 +12,20 @@ $direccion = $_POST["direccion"];
 $pais = $_POST["pais"];
 $numero = $_POST["numero"];
 
+
+$sql_insertar="INSERT INTO nuevo_usuario
+( fecha_sys, usuario, password, email, nombre, apellido, direccion, pais, numero)
+VALUES (now(),'".$usuario."','".$password."','".$email."','".$nombre."','".$apellido."','".$direccion."','".$pais."','".$numero."')";
+
+
+
+
+//ENVIAR A LA BD
+if($dbh->query($sql_insertar))
+{
+    print "INFORMACION REGISTRADA CORRECTAMENTE";
+}else
+{
+    print "ERROR AL REGISTRAR LA INFORMACION";
+}
 ?>
